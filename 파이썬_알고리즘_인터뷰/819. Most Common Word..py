6450a0ebf98eaac903063722d9f5107ee75502f5
allzeroyou <ekdudd119@naver.com>
2022-09-08 17:57:19 +0900

    0908-1

--- 파이썬_알고리즘_인터뷰/819. Most Common Word..py
+++ 파이썬_알고리즘_인터뷰/819. Most Common Word..py
@@ -1,8 +1,12 @@
 from typing import List
+import re
 
 class Solution:
     def mostCommonWord(self, paragraph: str, banned: List[str]) -> str:
+        paragraph=paragraph.lower()
+        paragraph=re.sub('[^a-z0-9]','',paragraph)
+        print(paragraph)
 
 
 s = Solution()
-print(s.mostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.", banned = ["hit"])
+print(s.mostCommonWord("Bob hit a ball, the hit BALL flew far after it was hit.", banned = ["hit"]))
