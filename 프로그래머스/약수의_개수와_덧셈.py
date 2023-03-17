5fd6b0ea21943b3c20a9fe1152c3b7d019f7279f
allzeroyou <ekdudd119@naver.com>
2023-03-17 12:09:20 +0900

    0317

---
+++ 프로그래머스/약수의_개수와_덧셈.py
@@ -0,0 +1,40 @@
+def solution(left, right):
+    # left~right까지 모든 수들 중에서, 약수의 개수가 짝수 -> +
+    # 약수의 개수가 홀수 -> -
+
+    # e.g. left: 13, right: 17 -> result: 43
+    # 13 약수: 1, 13 => 짝수
+    # 14 약수: 1,2,7,14 => 짝수
+    # 15 약수: 1,3,5,15 => 짝수
+    # 16 약수: 1,2,4,8,16 => 홀수
+    # 17 약수: 1, 17 => 짝수
+
+    # 13+14+15-16+17 = 43
+
+    divisor_list = []
+    answer = 0
+
+    for i in range(left, right + 1):
+        for j in range(1, i + 1):
+            if (i % j == 0):
+                divisor_list.append(j)
+        if len((divisor_list)) % 2 == 0:
+            answer += i
+
+        else:
+            answer -= i
+            divisor_list.clear()
+
+    # 다른 사람의 풀이- 제곱수는 약수의 개수가 홀수임을 이용.
+    # 4: 1, 2, 4
+    # 9: 1, 3, 9
+    # 16: 1,2,4,8,16
+
+    # for i in range(left, right+1):
+    #     if int(i**0.5)==int(i**0.5):
+    #         answer-=i
+    #     else:
+    #         answer+=i
+    # return answer
+
+    return answer
