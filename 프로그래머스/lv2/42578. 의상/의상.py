330358a0d782b939244e977766c01d6ef6533a2a
DaYeong YOO <0fcoursewhynot@duksung.ac.kr>
2023-05-15 12:37:46 +0900

    [level 2] Title: 의상, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.py
+++ 프로그래머스/lv2/42578. 의상/의상.py
@@ -1,13 +1,22 @@
 def solution(clothes):
-    # 1. 옷을 종류별로 구분하기
-    hash_map = {}
-    for clothe, type in clothes:
-        hash_map[type] = hash_map.get(type, 0) + 1
-        
-    # 2. 입지 않는 경우를 추가하여 모든 조합 계산하기
-    answer = 1
-    for type in hash_map:   
-        answer *= (hash_map[type] + 1)
+    # 각 행 [의상 이름, 의상 종류]로 이뤄짐
+    d = {}
+    cnt = 0
+    res = 1
+    for i in clothes:
+        if i[1] not in d:
+            d[i[1]] = 1
+        else:
+            d[i[1]]+=1
+    # print(d)
+    # for j in d:
+    #     if len(d) == 1:
+    #         return d[j]
+    #     else:
+    #         cnt += (d[j])
+    # print(cnt)
+    for k in d.values():
+        res *= (k+1) # 계수들의 합
+    return res-1 # x^3의 계수 빼기
+
     
-    # 3. 아무종류의 옷도 입지 않는 경우 제외하기
-    return answer - 1
