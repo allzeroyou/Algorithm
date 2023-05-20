729e87a91aa1eaecabd3e0a70c44427d6b68f09e
DaYeong YOO <0fcoursewhynot@duksung.ac.kr>
2023-05-20 23:11:26 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -0,0 +1,15 @@
+def solution(citations):
+    answer = 0
+    # index가 인용 횟수인 리스트 생성
+    res=[0]*(max(citations)+1)
+    # 인용 횟수가 c번인 논문의 개수 확인
+    for c in range(max(citations)+1):
+        cnt=0
+        for i in citations:
+            if i>=c:
+                cnt+=1
+        res[c]=cnt
+    for idx, val in enumerate(res):
+        if idx<=val:
+            answer=idx            
+    return answer
