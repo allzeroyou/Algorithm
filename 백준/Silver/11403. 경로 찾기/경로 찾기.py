c034c7fc183d9a28069483ceeafdaae190a3b34d
DaYeong YOO <0fcoursewhynot@duksung.ac.kr>
2023-08-11 13:18:30 +0900

    [Silver I] Title: 경로 찾기, Time: 204 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,23 @@
+# 정점 개수
+N = int(input())
+ 
+inputMap = [[0 for col in range(0, N)] for row in range(0, N)]
+ 
+for i in range(0, N) :
+    for j, m in enumerate(map(int, input().split())) :
+        inputMap[i][j] = m
+ 
+#플로이드 워셜 알고리즘(Floyd Warshall Algorithm) 이용
+# 모든 정점에 대한 경로를 계산하는 알고리즘
+for k in range(0, N) :
+    for i in range(0, N) :
+        for j in range(0, N):
+            if inputMap[i][k] and inputMap[k][j] :
+                inputMap[i][j] = 1
+ 
+for i in range(0, N) :
+    _str = ""
+    for j in range(0, N) :
+        _str += str(inputMap[i][j]) +  " "
+    print(_str)
+ 
