91a462089983a5c38dc4621171dbc8cdf75607ec
DaYeong YOO <0fcoursewhynot@duksung.ac.kr>
2023-08-14 21:06:48 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 300 ms, Memory: 47028 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -1,16 +1,24 @@
 import sys
 
-# 빠른 입력
 input = sys.stdin.readline
-
 n = int(input())
 
-board=[]
+dic = {}
+
 for _ in range(n):
-	x, y = map(int, input().split())
-	board.append([x,y])
- 
-sorted_board = sorted(board, key=lambda s: (s[1], s[0]))
-                      
-for s in sorted_board:
-	print(s[0], s[1])
+    x, y = map(int, input().split())
+
+    if y in dic:
+        dic[y].append(x)  # 1, 2
+    else:
+        dic[y] = [x]
+
+key_list = list(dic.keys())
+
+key_list.sort()
+
+for key in key_list:
+    ans = dic[key]
+    ans.sort()
+    for a in ans:
+        print(f"{a} {key}")
