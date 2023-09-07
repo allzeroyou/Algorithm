839cffa99adc47329c60d22656873b8a6537709e
DaYeong YOO <0fcoursewhynot@duksung.ac.kr>
2023-09-07 21:27:36 +0900

    [Gold V] Title: LCS, Time: 588 ms, Memory: 56440 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -7,7 +7,7 @@ x_len = len(x)
 y_len = len(y)
 
 # 2차원 배열 dp 테이블 생성
-dp = [[0] * (y_len + 1) for _ in range(x_len + 1)]  # 1-based
+dp = [[0 for _ in range(y_len + 1)] for _ in range(x_len + 1)]  # 1-based
 
 for i in range(1, x_len + 1):
     for j in range(1, y_len + 1):
@@ -15,5 +15,6 @@ for i in range(1, x_len + 1):
         if x[i - 1] == y[j - 1]:  # LCS가 맞음
             dp[i][j] = dp[i - 1][j - 1] + 1  # X[i], Y[j]를 포함하기 전의 LCS에 1을 더한 값과 같음
         else:  # 문자열 X의 i번째 문자와 문자열 Y의 j번째 문자가 서로 다른 경우
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) # LCS가 아님
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])  # LCS가 아님
+            
 print(dp[x_len][y_len])
