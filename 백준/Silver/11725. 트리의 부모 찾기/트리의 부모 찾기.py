79dfedea2b281f46bc341721b7fadbfc4eed9542
DaYeong YOO <0fcoursewhynot@duksung.ac.kr>
2023-09-12 08:50:10 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 368 ms, Memory: 54084 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -1,32 +1,41 @@
+'''
+루트: 1
+트리 순회 -> 노드의 개수가 십만이므로 -> bfs이용
+'''
+import sys
 from collections import deque
 
-n = int(input())
-visited = [False for _ in range(n + 1)]  # 각 노드가 방문된 정보를 리스트 자료형으로 표현(1차원 리스트)
-answer = [0 for _ in range(n + 1)]  # 정답 담을 리스트
-graph = [[0] for _ in range(n + 1)]  # 트리
+input = sys.stdin.readline
 
+n = int(input())
+# 트리 만들기
+graph = [[0] for _ in range(n + 1)]  # 노드는 n까지 이므로 n+1
+# 방문 체크
+visited = [False for _ in range(n + 1)]  # 1차원 리스트
+# 정답 리스트
+answer = [0 for _ in range(n + 1)]
+
+# 입력
 for _ in range(n - 1):  # 1은 제외
     x, y = map(int, input().split())
     graph[x].append(y)
-    graph[y].append(x)  # 무방향은 양방향임.
+    graph[y].append(x)  # 무방향 == 양방향
 
 
 def bfs(graph, start, visited):
-    # 큐(Queue) 구현을 위해 deque 라이브러리 사용
-    que = deque([start])
-    # 현재 노드를 방문 처리
+    que = deque([start])  # start point
     visited[start] = True
-    # 큐가 빌때까지 반복
-    while que:
-        v = que.popleft()
-        # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
+    while que:  # 큐가 빌때까지
+        v = que.popleft()  # 큐에서 노드(정점)를 꺼낸 다음
+        # 해당 원소와 연결된, 아직 방문하지 않은 노드를 큐에 삽입
         for i in graph[v]:
             if not visited[i]:
-                answer[i] = v
                 que.append(i)
+                answer[i] = v  # 정답 리스트에 담는다
                 visited[i] = True
 
 
+# 루트 노드는 1
 bfs(graph, 1, visited)
 
 for i in range(2, n + 1):  # 2번 노드부터 시행
