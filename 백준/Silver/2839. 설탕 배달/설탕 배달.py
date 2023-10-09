7610b45e1c183e741a85d8d4504ccef51ad8dcbe
DaYeong YOO <0fcoursewhynot@duksung.ac.kr>
2023-10-09 23:21:15 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,14 +1,15 @@
 n = int(input())
-k = [3, 5]
+cnt = 0
 
-d = [5001] * (n + 1)
-d[0] = 0
-# dp- bottom-up
-for i in range(len(k)):  # 설탕 단위 체크
-    for j in range(k[i], n + 1):
-        if d[j - k[i]] != 5001:
-            d[j] = min(d[j], d[j - k[i]] + 1)
-if d[n] == 5001:
-    print(-1)
+while n >= 0:
+    if n % 5 == 0: # n==0일때 걸림
+        cnt += (n // 5)
+        print(cnt)
+        break
+    else:
+        n -= 3
+        cnt += 1
 else:
-    print(d[n])
+    print(-1)
+
+
