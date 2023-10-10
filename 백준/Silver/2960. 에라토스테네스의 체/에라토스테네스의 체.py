7a300dd11c7cf98eaf426bdab35381353f4aa7f7
DaYeong YOO <0fcoursewhynot@duksung.ac.kr>
2023-10-10 12:20:29 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.py
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.py
@@ -8,19 +8,21 @@ cnt = 0  # 순서 카운트
 # 2부터 n 까지
 lst = [i for i in range(2, n + 1)]
 
-while len(lst) != 0:
-    # 가장 작은 수 : p
+while lst:
     min_num = lst[0]
     baesu_lst = []
 
-    for j in range(len(lst)):  # 2~7
-        if lst[j] % lst[0] == 0:
-            # lst.remove(lst[j])
+    for j in range(len(lst)):
+        if lst[j] % lst[0] == 0: # p의 배수라면
             baesu_lst.append(lst[j])
             cnt += 1
             if cnt == k:
-                print(baesu_lst[-1])
-                break
+                print(lst[j])
+                break # for문 탈출
     if cnt == k:
-        break
+        break # while 탈출
+    # lst - baesu lst
     lst = [x for x in lst if x not in baesu_lst]
+
+
+
