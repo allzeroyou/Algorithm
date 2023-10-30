82f60621fa35456ccf4fb276dde309bb9fcfadbb
DaYeong YOO <0fcoursewhynot@duksung.ac.kr>
2023-10-30 22:09:03 +0900

    [Silver III] Title: N과 M (1), Time: 148 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,14 +1,23 @@
-# nPm
-from itertools import permutations
+# 백트래킹=> dfs 가지치기
+def dfs():
+  if len(nums) == m:  # 재귀함수로 반복 -> 함수 출력 조건을 먼저 걸어준다.
+    print(' '.join(map(str, nums)))
+    return
+  for i in range(1, n+1):
+    if visited[i]: # 이미 방문했다면..
+      continue
+    visited[i]=True
+    nums.append(i)
+    dfs()
+    nums.pop()
+
+    visited[i]=False
+    
+    
+  
+    
 
 n, m = map(int, input().split())
-nums = []
-for i in range(1, n+1):
-    nums.append(i)
-
-perm = list(permutations(nums, m))
-
-for i in perm:
-    for j in i:
-        print(j, end=' ')
-    print()
+nums = [] # 숫자가 담길 스택
+visited= [False]*(n+1) # 이미 방문한 경우라면 제외하자.
+dfs()
