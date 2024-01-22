cdb4da552470f9f35176eaff6038329ea2f8f6ab
DaYeong YOO <0fcoursewhynot@duksung.ac.kr>
2024-01-22 15:13:04 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -0,0 +1,12 @@
+def solution(numbers):
+    answer = ''
+
+    def three_number(num):
+        str_num = str(num)
+        three = (str_num * 3)
+        return three
+
+    numbers.sort(key=lambda x: three_number(x), reverse=True)
+
+    answer = ''.join(map(str, numbers))
+    return str(int(answer))
