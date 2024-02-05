def0d50a0ff1d0cd498118910e143be52e27c419
DaYeong YOO <0fcoursewhynot@duksung.ac.kr>
2024-02-05 14:43:14 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.py
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.py
@@ -1,27 +1,13 @@
-# 아홉 개의 수 중 합이 100이 되는 일곱개의 수?
-import random
+from itertools import combinations
 
-nine = []
-for i in range(9):
-    nine.append(int(input()))
+num = [int(input()) for _ in range(9)]
 
-sum_value = 0
-seven = []
+small = []
+total = 0
 
-# 1. 아홉 개의 수의 합을 구한다.
-# 2. 이때 아홉 개의 수중에서 2개를 추출
-# 3. 추출한 값을 뺐을때 100이 되는 일곱개의 수를 출력
+pers = list(combinations(num, 7))
 
-for n in nine:
-    sum_value += n
-
-flag = 1
-while (flag == 1):
-    two = random.sample(nine, 2)
-    if sum_value-sum(two) == 100:
-        seven = [n for n in nine if n not in two]
-        break
-
-# print(seven)
-for _ in seven:
-    print(_)
+for per in pers:
+    if sum(per) == 100:
+        for p in per:
+            print(p)
