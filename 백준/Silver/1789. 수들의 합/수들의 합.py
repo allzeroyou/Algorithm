b8df5fc77d157b3fa8c35f942afb0d9b32a4ed7f
DaYeong YOO <0fcoursewhynot@duksung.ac.kr>
2024-02-05 18:27:06 +0900

    [Silver V] Title: 수들의 합, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.py
+++ 백준/Silver/1789. 수들의 합/수들의 합.py
@@ -1,26 +1,17 @@
-# 자연수
-# 1 ≤ S ≤ 4,294,967,295
-
 s = int(input())
+# 개수 세기
+total = 0
+cnt = 0
 
-# 서로 다른 n개의 자연수의 합이 s
-# n의 최댓값?
+for i in range(1, s + 1):
+    cnt += 1
+    total += i
 
-
-sum = 0
-cnt_n = 0  # n의 최댓값 세기 위한 변수
-
-for i in range(1, s+1):
-    sum += i
-    cnt_n += 1
-
-    if sum > s:
+    if total > s:  # total==s이 아닌 이유는 s를 초과하는 경우를 감지하고 그때까지의 개수를 출력하기 위함임.
         break
 
-if s == 1:  # 자연수 1일때는 예외처리
-    print(cnt_n)
+# 예외 처리
+if s == 1:
+    print(1)
 else:
-    print(cnt_n-1)  # s를 초과하는 것이므로 -1
-
-
-# s까지 하나씩 증가해가면서 더하고 만약 s보다 크다면.. 반복 종료
+    print(cnt - 1)  # s 초과하는 경우이므로, -1 해줌.
