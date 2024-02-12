d4ba29461329ce5ee72d393aaea2ae62dbc701e1
DaYeong YOO <0fcoursewhynot@duksung.ac.kr>
2024-02-12 22:52:35 +0900

    [Silver III] Title: N과 M (1), Time: 140 ms, Memory: 35556 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,23 +1,14 @@
-# 백트래킹=> dfs 가지치기
-def dfs():
-  if len(nums) == m:  # 재귀함수로 반복 -> 함수 출력 조건을 먼저 걸어준다.
-    print(' '.join(map(str, nums)))
-    return
-  for i in range(1, n+1):
-    if visited[i]: # 이미 방문했다면..
-      continue
-    visited[i]=True
-    nums.append(i)
-    dfs()
-    nums.pop()
-
-    visited[i]=False
-    
-    
-  
-    
+# 자연수 n,m
+# 1~n까지 자연수 중 중복 없이 m개를 고른 수열.
+from itertools import combinations, permutations
 
 n, m = map(int, input().split())
-nums = [] # 숫자가 담길 스택
-visited= [False]*(n+1) # 이미 방문한 경우라면 제외하자.
-dfs()
+# 순서 체크-> 순열
+num = [x for x in range(1, n + 1)]
+
+lst = [list(permutations(num, m))]
+# 출력 조건: 숫자 간 공백, 오름차순
+
+for ls in lst:
+    for l in ls:
+        print(*l)
