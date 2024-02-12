2e6480331a74ac28c1a9486ad4cf85590164c262
DaYeong YOO <0fcoursewhynot@duksung.ac.kr>
2024-02-12 22:56:35 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,21 +1,18 @@
 n, m = map(int, input().split())
+arr = []
 
-nums = []  # 숫자가 담길 스택
 
-
-def dfs(depth, i):
-    if depth == m:  # 재귀함수로 반복 -> 함수 출력 조건을 먼저 걸어준다.
-        print(' '.join(map(str, nums)))
+def dfs(start):
+    # 길이가 m이 되었다면
+    if len(arr) == m:
+        print(' '.join(map(str, arr)))
         return
-
-    for j in range(i + 1, n + 1):
-        if not visited[j]:  # 사용하지 않은 수라면
-            visited[j] = True  # 사용한 수 체크
-            nums.append(j)  # 수열에 추가
-            dfs(depth + 1, j)  # 다음 +1 번째 수열을 위해 재귀함수 호출
-            visited[j] = False  # 이전 상태로 돌아가기
-            nums.pop()
+    # 1~n 중에서 1개 뽑기
+    for i in range(start, n + 1):
+        if i not in arr:  # ab, ba는 같은 코드임.
+            arr.append(i)
+            dfs(i + 1)
+            arr.pop()
 
 
-visited = [False] * (n + 1)  # 이미 방문한 경우라면 제외하자.
-dfs(0, 0)
+dfs(1)
