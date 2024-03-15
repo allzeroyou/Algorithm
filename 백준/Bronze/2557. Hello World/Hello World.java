57a7885a90c3ba45ce1e15f85e912e75978f1bc9
DaYeong YOO <0fcoursewhynot@duksung.ac.kr>
2024-03-15 18:44:28 +0900

    [Bronze V] Title: Hello World, Time: 116 ms, Memory: 13848 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,7 +1,9 @@
-
-
 public class Main {
     public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        System.out.println(a+b);
         System.out.println("Hello World!");
     }
 }
