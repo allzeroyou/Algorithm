9e9f0dc2e1f31b693d28480650eb040f975877ef
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2024-05-18 23:58:28 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 193 ms, Memory: 52,004 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -1,37 +1,28 @@
+def dfs(index, count) :
+    global result
+    if count == int(cnt) :
+        result = max(int(''.join(data)), result)
+        return
+    for now in range(index, len(data)) :
+        for max_idx in range(now + 1, len(data)) :
+            if data[now] <= data[max_idx] :
+                data[now], data[max_idx] = data[max_idx], data[now]
+                dfs(now, count + 1)
+                data[now], data[max_idx] = data[max_idx], data[now]
+
+    if result == 0 and count < int(cnt) :
+        extra = (int(cnt) - count) % 2
+        # 짝수라면 그대로, 홀수라면 한 번 변경
+        if extra == 1 : # 홀수라면
+            data[-1], data[-2] = data[-2], data[-1]
+        dfs(index, int(cnt))
+
 t = int(input())
 
+for tc in range(1, t + 1) :
+    data, cnt = input().split()
+    data = list(data)
+    result = 0
+    dfs(0, 0)
 
-def dfs(cnt):
-    global ans
-    # 바꿀 수 있는 기회가 없다면
-    if cnt == 0:
-        ans = max(int(''.join(nums)), ans)
-        return
-    # 교환
-    for i in range(len(nums)):
-        for j in range(i + 1, len(nums)):
-            nums[i], nums[j] = nums[j], nums[i]
-            # 백트래킹을 위해 합친다.
-            tmp_key = ''.join(nums)
-            if visited.get((tmp_key, cnt - 1), 1):
-                # 딕셔너리안에 없다면 아직 사용 안한 것 -> 0으로 체크
-                visited[(tmp_key, cnt - 1)] = 0
-                # dfs 실행
-                dfs(cnt - 1)
-            # 다시 바꾼 값을 원상복귀
-            nums[i], nums[j] = nums[j], nums[i]
-
-
-for tc in range(1, t + 1):
-    # 숫자판 정보, 교환횟수
-    temp, cnt = input().split()
-    # 리스트 화, 정수형 화
-    nums = list(temp)
-    cnt = int(cnt)
-    # 정답
-    ans = -1
-    # 백트래킹용
-    visited = {}
-    dfs(cnt)
-
-    print(f'#{tc} {ans}')
+    print('#%d %d' % (tc, result))
