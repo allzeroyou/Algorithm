aa1977d8f8c8358e3a2544137661aa1cf46b2b25
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2024-07-26 10:38:55 +0900

    [level 3] Title: 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
+++ 프로그래머스/3/298519. 특정 조건을 만족하는 물고기별 수와 최대 길이 구하기/특정 조건을 만족하는 물고기별 수와 최대 길이 구하기.sql
@@ -1,16 +1,19 @@
 -- 코드를 작성해주세요
--- 길이가 10 이하: NULL -> 10으로 취급해서 평균 길이 구하기(case문 or coalesce 함수)
--- 1. type으로 그룹화(select 절에는 1.집계함수 or 2.group by에 명시된 컬럼만 사용 가능)
--- 2. 
+-- 평균 33cm 이상인 물고기-> 종류별 분류
+-- 잡은수, 최대길이, 물고기 종류 출력
+-- 물고기 종류에 대해 오름차순 정렬
+-- 물고기의 길이가 10cm 이하일 경우에는 LENGTH 가 NULL -> 10cm로 취급
 
 SELECT
     COUNT(*) AS FISH_COUNT,
-    MAX(CASE WHEN LENGTH IS NULL THEN 10 
+    MAX(CASE
+        WHEN LENGTH IS NULL THEN 10 
         ELSE LENGTH END) AS MAX_LENGTH,
-    FISH_TYPE 
+    FISH_TYPE
 FROM FISH_INFO
 GROUP BY FISH_TYPE
-HAVING
-    AVG(CASE WHEN LENGTH IS NULL THEN 10 
-        ELSE LENGTH END) >= 33
+HAVING AVG(CASE
+        WHEN LENGTH IS NULL THEN 10 
+        ELSE LENGTH
+    END) >= 33
 ORDER BY FISH_TYPE;
