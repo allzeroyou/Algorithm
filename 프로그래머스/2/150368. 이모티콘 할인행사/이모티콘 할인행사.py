575a90afff1f0a904afcd1f91ca86c78898f7fb0
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2024-08-06 11:08:43 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 2144.09 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.py
@@ -1,10 +1,11 @@
-# 가능한 할인율 조합 -> 이모티콘 플러스 서비스 가입자 수 늘리기
-# 그 다음으로, 이모티콘 판매액 최대로 늘리기
+# 우선순위
+# 1.이모티콘 플러스 서비스 가입자 수 늘리기
+# 2.이모티콘 판매액 최대로 늘리기
 
-# 1. 가능한 할인율 조합 생성
+# 1. 가능한 할인율 조합
 # 10, 20, 30, 40% 중 하나의 할인율 적용(4^m 가지)
 
-# 2. 각 조합에 대한 가입자 수와 판매액 계산
+# 2. 각 조합에 대한 서비스 가입자 수/ 판매액 계산
 # 할인율 조합에 대해 이모티콘 구매 or 서비스 가입할지 계산
 
 # 3. 최적 결과 선택
@@ -12,8 +13,11 @@
 from itertools import product
 
 def solution(users, emoticons):
+    # 할인율
     dc_rate = [10, 20, 30, 40]
+    # 가입자수
     max_sub = 0
+    # 판매액
     max_buy = 0
     
     # 모든 할인율 조합 계산
@@ -26,7 +30,7 @@ def solution(users, emoticons):
             # 각 사용자가 구매할 이모티콘 총액 계산
             for i in range(len(emoticons)):
                 if dc[i] >= user_rate:
-                    total += emoticons[i] * (100-dc[i])//100
+                    total += emoticons[i] * (100-dc[i])/100
             # 총액이 기준 가격을 넘으면 이모티콘 플러스 가입
             if total >= user_price:
                 sub += 1
