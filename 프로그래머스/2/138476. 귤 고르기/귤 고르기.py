fd6d0428ae4a7e113a67efce509f28853a23fe70
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2024-08-10 20:44:59 +0900

    [level 2] Title: 귤 고르기, Time: 21.75 ms, Memory: 21.7 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,20 +1,29 @@
-from collections import Counter
+# 귤 중 k개를 골라 상자에 담기
+# k: 판매할 귤 개수
+# 귤을 크기별 분류 -> 서로 다른 종류의 수를 최소화
+from collections import Counter 
 
 def solution(k, tangerine):
-   # 1. 귤 크기별 빈도 계산
+    # 뽑은 귤 종류
+    answer = 0
+    
+    # 귤 빈도수 계산
     counter = Counter(tangerine)
     
-    # 2. 빈도순 정렬 (내림차순)
+    # 빈도수 내림차순 정렬
     frequencies = sorted(counter.values(), reverse=True)
     
-    # 3. 가장 많이 나오는 귤부터 골라서 k개 채우기
-    count = 0
-    selected = 0
-    
+    # 귤 뽑기
+    tan_cnt = 0 # 뽑은 귤 개수
     for freq in frequencies:
-        count += freq
-        selected += 1
-        if count >= k:
+        tan_cnt += freq
+        answer += 1
+        if tan_cnt >= k:
             break
+    return answer
+        
+        
     
-    return selected
+    
+    
+    return answer
