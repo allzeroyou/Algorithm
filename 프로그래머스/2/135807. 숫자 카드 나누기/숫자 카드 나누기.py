61c121049f08644c1499f3739559a7d5c579a840
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2024-08-11 14:55:46 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 97.66 ms, Memory: 53.3 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
@@ -1,26 +1,32 @@
-# 주어진 숫자의 모든 약수를 찾는 함수
-# n의 제곱근(√n)까지만 검사하면, 더 적은 계산으로 모든 약수를 찾을 수 있습니다.
-def get_divisors(n):
-    divisors = set()  # 약수를 저장할 집합, 중복을 방지하기 위해 집합 사용
-    for i in range(1, int(n**0.5) + 1): # 1부터 n의 제곱근까지 반복
-        if n % i == 0: # i가 n의 약수인 경우
-            divisors.add(i) # i를 약수 집합에 추가
-            divisors.add(n // i) # n을 i로 나눈 몫도 약수이므로 추가
-    return divisors
+import math
+from functools import reduce # functools 모듈에서 reduce 함수만 사용하겠음
 
 def solution(arrayA, arrayB):
-    # arrayA와 arrayB의 최소값의 약수
-    divisorsA = get_divisors(min(arrayA))
-    divisorsB = get_divisors(min(arrayB))
+    answer = 0
+
+    # 주어진 배열의 최대공약수 구하는 함수
+    def get_gcd(arr):
+        return reduce(math.gcd, arr)
     
-    max_a = 0 # 구할 값
-    # 나눠지는지 확인 (all은 값이 참이어야지 참, any는 하나라도 참이면 참)
-    for d in divisorsA:
-        if all(a % d == 0 for a in arrayA) and not any(b % d == 0 for b in arrayB):
-            max_a = max(max_a, d)
-            
-    for d in divisorsB:
-        if all(b % d == 0 for b in arrayB) and not any(a % d == 0 for a in arrayA):
-            max_a = max(max_a, d)
-            
-    return max_a
+    # arrA, arrB 각각 최대 공약수 구하기
+    gcd_a = get_gcd(arrayA)
+    gcd_b = get_gcd(arrayB)
+    
+    # gcd가 다른 배열의 모든 원소를 나눌 수 없는지 체크
+    def is_div(gcd, arr):
+        for num in arr:
+            if num % gcd ==0:
+                return False
+        return True
+    # 조건에 맞는지 확인 후 결과 반환
+    if is_div(gcd_a, arrayB):
+        answer = gcd_a
+    if is_div(gcd_b, arrayA):
+        answer = max(answer, gcd_b)
+    
+    
+    
+    
+        
+        
+    return answer
