1258526acc546a5cf70608bf7cf947723940f5e0
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2024-09-17 22:57:22 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -0,0 +1,12 @@
+-- 코드를 입력하세요
+-- 입양 못간 동물 중, 가장 오래 보호소에 있었던 동물 3마리의 이름, 보호시작일
+
+SELECT I.NAME, I.DATETIME
+FROM ANIMAL_INS I
+WHERE NOT EXISTS (
+    SELECT *
+    FROM ANIMAL_OUTS O
+    WHERE I.ANIMAL_ID=O.ANIMAL_ID
+)
+ORDER BY I.DATETIME
+LIMIT 3;
