1116fe4ce365c3bdb4f1d40c4059663afd00c918
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2024-09-20 15:49:06 +0900

    [level 3] Title: 네트워크, Time: 0.23 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,42 +1,34 @@
-// 네트워크: 컴퓨터간 정보 교환할 수 있도록 연결된 형태
-// 컴퓨터 A, B가 직접적 연결
-// 컴퓨터 B, C가 직접적 연결 -> 컴퓨터 A,C도 간접적 연결
-// 컴퓨터 A,B,C는 모두 같은 네트워크 상에 위치함
-
-// 컴퓨터 개수: n, 정보가 담긴 2차원 배열: computers
-// 네트워크 개수 return
-// i번, j번 컴퓨터 연결: computers[i][j]=1
-// 본인: computers[i][i]=1
-
-// == 연결요소 개수 찾기
-// 모든 컴퓨터를 돌면서 방문 안했다면 해당 컴퓨터로부터 연결된 컴퓨터 찾아서 cnt +1
+// 네트워크: 연결된 형태
+// a-b, b-c -> a-c
+// a,b,c는 모두 같은 네트워크
+// 2차원 배열에 약한듯..
+// computers[i][j]=1 는 연결된거
 
 class Solution {
-    boolean[] visited; // 방문 배열
+    static boolean[] visited;
+    
     public int solution(int n, int[][] computers) {
-        int answer = 0; // 네트워크 수
+        // 이미 방문한 컴퓨터는 다시 방문 x
         visited = new boolean[n];
+        int answer = 0;
         
-        for(int i =0; i<n; i++){
-                if(!visited[i]){ // 방문 체크
-                    dfs(i, computers);
-                    answer ++; 
-                }
+        for(int i=0; i<n; i++){
+            if(!visited[i]){
+                dfs(i, computers); 
+                answer++; // 네트워크 개수 증가
             }
+        }
         return answer;
     }
-        
-    
-    void dfs(int v, int[][] computers){
+    public void dfs(int v, int[][] computers){
+        // 방문체크
         visited[v]=true;
         
         for(int i=0; i<computers.length; i++){
-            if(computers[v][i]==1 && !visited[i]){ // 현재 행의 각 열 순회
+            // 연결되어있고 방문하지 않았다면
+            if(computers[v][i]==1 && !visited[i]){
                 dfs(i, computers);
-                
             }
         }
     }
 }
-
-
