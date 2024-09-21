43aaf42189099458fd792dcd7ab8ad45370a16db
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2024-09-21 09:43:16 +0900

    [level 3] Title: 여행경로, Time: 75.83 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/README.md
+++ 프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164) 
 
 ### 성능 요약
 
-메모리: 104 MB, 시간: 81.68 ms
+메모리: 114 MB, 시간: 75.83 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 14일 00:08:33
+2024년 09월 21일 09:43:13
 
 ### 문제 설명
 
--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,40 +1,40 @@
-// 주어진 항공권을 모두 이용해 여행경로 짜기
-// "ICN"에서 출발
-// tickets: 항공권 정보 담긴 2차원 배열
-// 방문하는 공항 경로를 배열로 담아 return
-
-// 가능한 경로가 2개 이상일 경우 알파벳 순서가 앞서는 경로 return
-// [a,b]->[b,a] 이렇게 이어져야 함.
+// 완전탐색 -> 모든 가능한 경로 구하기
+// 정렬(알파벳 순서가 앞서는 경로)
+// 가장 첫번째 요소 리턴
 import java.util.*;
 
 class Solution {
-    static Queue<String> pq = new PriorityQueue<>(); // 문자열 오름차순 정렬용
-    static boolean[] visited; // 방문 배열
+    static boolean[] visited; // 방문 체크
+    static List<String>result = new ArrayList<>();
     
     public String[] solution(String[][] tickets) {
+        String[] answer = {};
+        // 변수세팅
         visited = new boolean[tickets.length];
-        dfs(tickets, "ICN", 0, "ICN"); // 처음시작은 ICN
-        // pq에 들어온 값 중 첫번째 값이 최적임
-        String[] answer = pq.peek().split(",");
         
-        return answer;
+        // 완탐 및 정렬
+        dfs(tickets, "ICN", "ICN", 0);
+        
+        Collections.sort(result);
+        
+        String[] ans = result.get(0).split(" ");
+        
+        return ans;
     }
-    public void dfs(String[][] tickets, String curCity, int cnt, String path){
-        // 재귀 종료 조건
-        if(cnt==tickets.length){
-            // pq에 전체 여행 경로 추가
-            pq.add(path);
+    public void dfs(String[][] tickets, String start, String route, int idx){
+        // 종료 조건
+        if(idx==tickets.length){
+            result.add(route);
             return;
         }
+        // 백트래킹
         for(int i=0; i<tickets.length; i++){
-            if(!visited[i] && curCity.equals(tickets[i][0])){ // 현재 도시가 다음 여행 경로의 도착지
-                // 방문체크
+            if(tickets[i][0].equals(start) && !visited[i]){
                 visited[i]=true;
-                // 그다음 경로 탐색
-                dfs(tickets, tickets[i][1], cnt+1, path+","+tickets[i][1]);
-                // 모든 배열 순회시 방문배열 체크 초기화(백트래킹)
+                dfs(tickets, tickets[i][1],route+" "+tickets[i][1], idx+1 );
                 visited[i]=false;
             }
         }
+        return;
     }
 }
