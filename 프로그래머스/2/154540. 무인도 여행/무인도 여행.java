c03cdb13dbefe3c376a30cdb1ee90b20255f9cf2
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2024-09-23 21:06:52 +0900

    [level 2] Title: 무인도 여행, Time: 14.88 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.java
@@ -1,45 +1,50 @@
-import java.util.*;
+// 'X': 바다, 1~9: 무인도-식량
+// 상,하,좌,우로 연결된 땅은 하나의 무인도
+// 각 칸 숫자 -> 식량/ 상,하,좌,우로 연결되는 칸에 적힌 숫자를 모두 합한 값=>해당 무인도에서 최대 며칠 머물 수 있는지?
+// 각 섬에서 최대 며칠씩 머물 수 있는지 배열에 오름차순으로 담아 return
+// 만약 지낼 수 있는 무인도 x -> -1 반환
 
+import java.util.*;
 class Solution {
-    int[] dx = {-1, 1, 0, 0};  // 상하좌우 이동을 위한 배열
-    int[] dy = {0, 0, -1, 1};
-    int m, n;  // 지도의 크기
-    
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+    static boolean[][] visited;
+    static int food = 0;
+
     public int[] solution(String[] maps) {
-        m = maps.length;
-        n = maps[0].length();
-        boolean[][] visited = new boolean[m][n];
-        List<Integer> islands = new ArrayList<>();
+        int[] answer = {};
+        // 방문 체크
+        visited = new boolean[maps.length][maps[0].length()]; // 2차원 배열
+        // 결과 저장용 리스트
+        List<Integer> res = new ArrayList<>(); 
         
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!visited[i][j] && maps[i].charAt(j) != 'X') {
-                    islands.add(dfs(maps, visited, i, j));
+        for(int i=0; i<maps.length; i++){
+            for(int j=0; j<maps[0].length(); j++){
+                if(!visited[i][j] && maps[i].charAt(j)!='X'){ // 방문 x
+                    food=0; // 식량 초기화
+                    dfs(maps, visited, i, j);
+                    res.add(food);
                 }
             }
         }
-        
-        if (islands.isEmpty()) {
+        if(res.isEmpty()){
             return new int[]{-1};
         }
-        
-        Collections.sort(islands);
-        return islands.stream().mapToInt(i -> i).toArray();
+        Collections.sort(res);
+        // List -> Array로 변환
+        return res.stream().mapToInt(i->i).toArray();
     }
-    
-    private int dfs(String[] maps, boolean[][] visited, int x, int y) {
-        visited[x][y] = true;
-        int sum = maps[x].charAt(y) - '0';
-        
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            
-            if (nx >= 0 && nx < m && ny >= 0 && ny < n && !visited[nx][ny] && maps[nx].charAt(ny) != 'X') {
-                sum += dfs(maps, visited, nx, ny);
+    public void dfs(String[] maps, boolean[][] visited, int x, int y ){
+        visited[x][y]=true; // 방문 처리
+        // 문자 -> 숫자
+        food += maps[x].charAt(y)-'0'; // 해당 좌표값의 숫자를 더하기
+        for(int i=0; i<4; i++){
+            int nx = x+dx[i];
+            int ny = y+dy[i];
+            // 범위 안, 방문 x, X가 아니라면
+            if(nx>=0 && nx<maps.length && ny>=0 && ny<maps[0].length() && !visited[nx][ny] && maps[nx].charAt(ny)!='X'){
+                dfs(maps, visited, nx, ny);
             }
         }
-        
-        return sum;
     }
 }
