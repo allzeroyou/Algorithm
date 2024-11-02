06c8c206a65412026ebb9031e15b47f2057d3bd6
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2024-11-02 12:56:03 +0900

    [level 3] Title: 단어 변환, Time: 0.50 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,34 +1,28 @@
-# begin, target, 단어의 집합: words
-# begin -> target으로 변환하는 가장 짧은 과정! => bfs
-
-# 한번에 한개의 알파벳만 바꿀수 o -> 단어들 순회하면서 하나씩 바꾸면서 일치하는지 체크
-
-from collections import deque
-
 def solution(begin, target, words):
     answer = 0
     
-    q = deque()
-    q.append((begin, 0)) # 시작노드, 단어깊이 삽입
-    # 방문 단어 체크
-    visited = [0]*len(words)
+    q = [(begin, 0)]
+    # 단어 중복 사용 x
+    visited = [False]*len(words)
     
-    while(q):
-        word, dep = q.popleft()
+    while q:
+        word, cnt = q.pop()
         
         for i in range(len(words)):
-            
-            if not(visited[i]): # 사용 안한 단어라면
-                # 그 단어가 알파벳 하나만 다른지 검사
-                cnt = 0
-                for j in range(len(word)):
-                    if word[j]!=words[i][j]:
-                        cnt+=1
-                if cnt == 1: # 하나만 다른 단어라면
-                    q.append((words[i], dep+1))
-                    visited[i]=1
-        if word == target:
-            answer = dep
-            break # while 탈출
-    
+            # 알파벳 하나만 다른거 비교 후 큐에 삽입
+            if not(visited[i]):
+                # 문자열 순회 가능하므로, 인덱스로 비교
+                flag = 0
+                
+                for j in range(len(word)): # 현재 단어
+                    if word[j] != words[i][j]: # 같지 않을때 카운트 중가 -> 카운트가 1인 단어 사용
+                        flag +=1
+                if flag == 1:
+                    q.append((words[i], cnt+1))
+                    visited[i]=True
+        # 탈출조건
+        if target == word:
+            answer = cnt
+            break
+                        
     return answer
