df9f33159cda5bc1cbd6f8c24a95994e7db882fe
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2024-11-04 21:43:30 +0900

    [level 2] Title: 전화번호 목록, Time: 483.19 ms, Memory: 46.8 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -3,14 +3,18 @@ def solution(phone_book):
     
     # 전화번호가 다른 전화번호의 접두어인지 판단
     # 접두어인지 어캐 판단하지
-    # in ?
-    # 시간복잡도 고려해야함. 그냥 for문 돌면 시초날수도
+    # 1.파이썬 문자열 시작 비교하는 함수 사용: startswith
+    # 2.해시맵으로 해당 전화번호를 저장해두고, temp로 한문자씩 붙여서 접두어 인지 체크!
     
-    phone_book.sort() # 정렬
-    
-    for i in range(len(phone_book)-1):
-        if phone_book[i+1].startswith(phone_book[i]):
-            answer= False
-
-
+    dic = {}
+    for n in phone_book:
+        if n not in dic:
+            dic[n]=1 # 같은 전화번호 중복 x else 생략
+    for phone in phone_book:
+        tmp = ''
+        for num in phone:
+            tmp += num
+            if tmp != phone and tmp in dic: # 자기자신이 아니고 접두어일때
+                return False
+            
     return answer
