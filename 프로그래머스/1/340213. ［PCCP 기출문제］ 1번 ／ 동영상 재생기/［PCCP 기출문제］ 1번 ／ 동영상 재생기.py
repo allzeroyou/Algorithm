e0e985d04eeb9cc5fce84de18d443d8fc1a6d697
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2024-11-22 15:14:30 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.06 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
@@ -1,54 +1,56 @@
 # 동영상 재생기
-# 3가지 기능: 10초 전으로  이동, 10초 후로 이동, 오프닝 건너뛰기 
-# 10초 전: "prev" 명령 -> 재생 위치: 현재 위치 - 10초(만약, 10초 미만일 경우 -> 처음 위치로 이동: 0분 0초)
-# 10초 후: "next" 명령 -> 재생 위치: 현재 위치 + 10초(만약, 10초 미만일 경우 -> 마지막 위치로 이동: 동영상 길이)
-# 오프닝 건너뛰기: 재생 위치가 오프닝 구간일 경우 자동으로 오프닝 끝나는 위치로 이동
-
-# 문자열 -> 숫자로 (대소 비교하기 위해)
-# : 기준으로 앞은 *60, 뒤는 그대로
-def str_to_time(temp):
-    tmp = temp.split(":")
-    mi= int(tmp[0])*60
-    sec = int(tmp[1])
-    str_to_time = mi + sec 
-    return str_to_time
+# 알고리즘: 구현(문제 조건 맞추기)
+# 3가지 기능 구현: +10초, -10초, 오프닝 건너뛰기
 
 def solution(video_len, pos, op_start, op_end, commands):
-    answer = ''
     
-    cur_time = str_to_time(pos)
-    ops_time = str_to_time(op_start)
-    ope_time = str_to_time(op_end)
-    video_time = str_to_time(video_len)
+    # : 기준 split하기
+    # 기준이 sec 이므로, 전체 sec로 맞추고 계산
     
-    for c in commands:
-        if ops_time<=cur_time <ope_time:
-            cur_time = ope_time
-                
-        if c == "next":
-            cur_time+=10
-            if ops_time<=cur_time <ope_time:
-                cur_time = ope_time
-            if video_time-cur_time < 10:
-                cur_time = video_time
-        elif c == "prev":
-            cur_time -=10
-            
-            if ops_time<=cur_time <ope_time:
-                cur_time = ope_time
-            if cur_time < 10:
-                cur_time = 0
+    lst =get_sec(video_len, pos, op_start, op_end)
+    # 비디오 길이, 현재위치, 오프닝시작, 오프닝끝
+    video, cur, start, end = lst
+    
+    for i in range(len(commands)):
+        if start<=cur<=end:
+            cur = end
+
+        if commands[i] == 'prev':
+            if cur < 10:
+                cur = 0
                 continue
-    print(cur_time)
-    
-    res_min=cur_time//60
-    res_sec = cur_time - (res_min)*60
+            cur -= 10
 
-    if len(str(res_min))<2:
-        res_min = "0"+str(res_min)
-    if len(str(res_sec))<2:
-        res_sec = "0"+str(res_sec)    
-    print(str(res_min)+":"+str(res_sec))
-
+            if start<=cur<=end:
+                cur = end
+        else: # next
+            if video-cur < 10:
+                cur = video
+                continue
+            cur += 10
+            if start<=cur<=end:
+                cur = end
+    #print(cur)
+    # 결과출력
+    # 초 -> 분으로 나누기
+    m = str(cur//60)
+    s = str(cur % 60)
     
-    return str(res_min)+":"+str(res_sec)
+    if len(m)<2:
+        m='0'+m
+    if len(s)<2:
+        s='0'+s
+    answer = ''
+    answer = m+':'+s
+    return answer
+
+
+def get_sec(t1, t2, t3, t4):
+    tmp = [t1, t2, t3, t4]
+    lst = []
+    for i in range(4):
+        m, s = tmp[i].split(':')
+        total = int(m)*60 + int(s)
+        lst.append(total)
+        
+    return lst
