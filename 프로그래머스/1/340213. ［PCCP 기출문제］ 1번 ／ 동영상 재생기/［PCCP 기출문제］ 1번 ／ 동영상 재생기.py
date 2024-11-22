d5a3a55954a791af6d70183bf5f381a5ebc35a54
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2024-11-22 15:22:27 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
@@ -6,30 +6,34 @@ def solution(video_len, pos, op_start, op_end, commands):
     
     # : 기준 split하기
     # 기준이 sec 이므로, 전체 sec로 맞추고 계산
-    
+    def get_sec(t1, t2, t3, t4):
+        tmp = [t1, t2, t3, t4]
+        lst = []
+        for i in range(4):
+            m, s = tmp[i].split(':')
+            total = int(m)*60 + int(s)
+            lst.append(total)
+
+        return lst
+
     lst =get_sec(video_len, pos, op_start, op_end)
+    
     # 비디오 길이, 현재위치, 오프닝시작, 오프닝끝
     video, cur, start, end = lst
     
-    for i in range(len(commands)):
+    # 공통 로직이므로 -> 아예 반복문 밖으로 빼기!
+    if start<=cur<=end:
+        cur = end
+        
+    for com in commands:
+        if com == 'prev':
+            cur = max(0, cur-10) # cur-10가 0미만일경우, 0으로 만드는 코드 기법!
+
+        elif com == 'next': # next
+            cur = min(video, cur+10) 
         if start<=cur<=end:
             cur = end
-
-        if commands[i] == 'prev':
-            if cur < 10:
-                cur = 0
-                continue
-            cur -= 10
-
-            if start<=cur<=end:
-                cur = end
-        else: # next
-            if video-cur < 10:
-                cur = video
-                continue
-            cur += 10
-            if start<=cur<=end:
-                cur = end
+            
     #print(cur)
     # 결과출력
     # 초 -> 분으로 나누기
@@ -42,15 +46,5 @@ def solution(video_len, pos, op_start, op_end, commands):
         s='0'+s
     answer = ''
     answer = m+':'+s
+    
     return answer
-
-
-def get_sec(t1, t2, t3, t4):
-    tmp = [t1, t2, t3, t4]
-    lst = []
-    for i in range(4):
-        m, s = tmp[i].split(':')
-        total = int(m)*60 + int(s)
-        lst.append(total)
-        
-    return lst
