f8b3fef8a8a770b28bc162d4f9f4b49f7aa0b2a2
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2024-12-11 14:34:23 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
@@ -1,44 +1,44 @@
-aeiou = ('a', 'e', 'i', 'o', 'u')  # 모음
+# 비밀번호 발음하기
+# 규칙
+# 1.모음: a, e, i, o, u 중 하나 반드시 포함
+# 2.모음 3개 or 자음 3개 연속으로 오면 안됨
+# 3.같은 글자가 연속적으로 2번 오면 안되나, ee와 oo는 허용
 
-while True:  # end가 나오기 전까지 반복
-    words = input()
-    if words == 'end':
+mo = ['a', 'e', 'i', 'o', 'u']
+
+while 1:
+    password = input()
+    if password == 'end':
         break
-    # 모음 개수
+    # 1. aeiou 중 하나 반드시 포함
     cnt = 0
-    # 자음, 모음 구별 & 1번째 조건 판단 위해 모음 개수 세기
-    for a in aeiou:
-        if a in words:
-            cnt += 1  # 모음 개수 +1
-    # 1. 모음이 하나도 없다면
-    if cnt < 1:
-        print(f'<{words}> is not acceptable.')  # 출력
-        # 다음 단어 탐색
+    for p in password:
+        if p in mo:
+            cnt += 1
+    if cnt == 0:
+        print(f'<{password}> is not acceptable.')
+        continue
+    flag = 0
+    # 2.
+    for i in range(len(password) - 2):
+        if password[i] in mo and password[i + 1] in mo and password[i + 2] in mo:
+            flag = 1
+        elif not (password[i] in mo) and not (password[i + 1] in mo) and not (password[i + 2] in mo):
+            flag = 1
+    if flag == 1:
+        print(f'<{password}> is not acceptable.')
         continue
 
-    # 2. 모음, 자음이 연속 3개라면
-    # 조건 카운트
-    s = 0
-    for i in range(len(words) - 2):
-        if words[i] in aeiou and words[i + 1] in aeiou and words[i + 2] in aeiou:
-            s = 1  # 연속 모음
-        elif not (words[i] in aeiou) and not (words[i + 1] in aeiou) and not (words[i + 2] in aeiou):
-            s = 1  # 연속 자음
-    if s == 1:
-        print(f'<{words}> is not acceptable.')  # 출력
-        continue
-
-    # 3. 같은 글자가 연속 2개인지, ee, oo만 허용
-    # 조건 카운트
-    t = 0
-    for i in range(len(words) - 1):
-        if words[i] == words[i + 1]:
-            if words[i] == 'e' or words[i] == 'o':  # ee, oo 라면
+    # 3.
+    flag2 = 0
+    for i in range(len(password) - 1):
+        if password[i] == password[i + 1]:
+            if password[i] == 'e' or password[i] == 'o':
                 continue
-            else:  # 그 외 연속 2개 단어라면
-                t = 1
-    if t == 1:
-        print(f'<{words}> is not acceptable.')  # 출력
+            else:
+                flag2 = 1
+    if flag2 == 1:
+        print(f'<{password}> is not acceptable.')
         continue
-    # 3개 조건 패스 시 적합한 비밀번호
-    print(f'<{words}> is acceptable.')  # 출력
+
+    print(f'<{password}> is acceptable.')
