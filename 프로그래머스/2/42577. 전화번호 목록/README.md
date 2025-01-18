aa67618a8547dda4c8195df83563d08c01bd0aea
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2025-01-18 22:10:34 +0900

    [level 2] Title: 전화번호 목록, Time: 350.46 ms, Memory: 99.3 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
 
 ### 성능 요약
 
-메모리: 46.8 MB, 시간: 483.19 ms
+메모리: 99.3 MB, 시간: 350.46 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 04일 21:43:28
+2025년 01월 18일 22:10:32
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,6 +1,39 @@
+// 전화번호가 다른 전화번호의 시작번호라면 -> false 반환
+// 없으면 true 반환
+
+// 1. 오름차순 정렬
+// 2. 첫번째 요소 -> n-1까지 요소까지 돌면서 접두어 있는지?
+
+import java.util.*;
+
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
-        return answer;
+        
+        
+        // 1. 정렬
+        Arrays.sort(phone_book);
+        // 2. for문 순회하면서 접두어 탐색
+        for (int i=0; i<phone_book.length -1; i++){
+            String cur = phone_book[i];
+            String nxt = phone_book[i+1];
+            
+            // 만약 다음 문자열이 더 짧다면 접두어 불가
+            if(nxt.length() < cur.length()){
+                continue;
+            }
+            // charAt()으로 문자열 인덱스 비교
+            boolean answer = true;
+
+            for(int j=0; j<cur.length(); j++){
+                if(cur.charAt(j) != nxt.charAt(j)){
+                    answer = false;
+                    break;
+                }
+            }
+            if(answer){
+                return false;
+            }
+        }
+        return true; // 접두어 아니라면 true 반환
     }
 }
