ac9689333e9ecf8a53b51300f2ef6be3c2aecf0c
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2025-01-18 22:25:13 +0900

    [level 2] Title: 전화번호 목록, Time: 263.46 ms, Memory: 232 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,39 +1,28 @@
-// 전화번호가 다른 전화번호의 시작번호라면 -> false 반환
-// 없으면 true 반환
-
-// 1. 오름차순 정렬
-// 2. 첫번째 요소 -> n-1까지 요소까지 돌면서 접두어 있는지?
-
 import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
+        boolean answer = true;
         
+        // HashMap 사용
+        HashMap<String, Integer> map = new HashMap<>();
         
-        // 1. 정렬
-        Arrays.sort(phone_book);
-        // 2. for문 순회하면서 접두어 탐색
-        for (int i=0; i<phone_book.length -1; i++){
-            String cur = phone_book[i];
-            String nxt = phone_book[i+1];
-            
-            // 만약 다음 문자열이 더 짧다면 접두어 불가
-            if(nxt.length() < cur.length()){
-                continue;
-            }
-            // charAt()으로 문자열 인덱스 비교
-            boolean answer = true;
-
-            for(int j=0; j<cur.length(); j++){
-                if(cur.charAt(j) != nxt.charAt(j)){
-                    answer = false;
-                    break;
+        // 1.전화번호부 만들기
+        for(int i=0; i<phone_book.length; i++){
+            map.put(phone_book[i], 1);
+        }
+        
+        // 2.현재 전화번호의 접두어가 map 에 있는지 체크.
+        for(int i=0; i<phone_book.length; i++){
+            for(int j=1; j<phone_book[i].length(); j++){
+                // 전화번호에서 접두어가 포함되는지 체크: substring
+                if(map.containsKey(phone_book[i].substring(0,j))){
+                    return false;
                 }
             }
-            if(answer){
-                return false;
-            }
+            
         }
-        return true; // 접두어 아니라면 true 반환
+         
+        return answer;
     }
 }
