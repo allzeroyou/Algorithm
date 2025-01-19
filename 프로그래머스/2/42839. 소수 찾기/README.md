e162cc31eeeb2c8510b3f6ebc03a006101b1ddac
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2025-01-19 18:10:03 +0900

    [level 2] Title: 소수 찾기, Time: 31.00 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
-메모리: 82.2 MB, 시간: 33.94 ms
+메모리: 84.6 MB, 시간: 31.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 13일 23:50:57
+2025년 01월 19일 18:10:00
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,57 +1,54 @@
-// 한자리 숫자 적힌 종이조각 흩어짐
-// 흩어진 종이 조각 붙여 소수 몇 개 만들 수 있는지?
-// numbers: 각 종이 조각에 적힌 숫자가 적힌 문자열
-// 종이로 만들 수 있는 소수 몇개 -> return
-// numbers: 1<=문자열 길이<=7, 0~9 숫자로 이뤄짐
-
-// numbers 문자열 -> 한 자릿수로 쪼개서 배열에 저장
-// 배열 순회하면서 
-// 숫자 조합을 통해 소수 만들어야 함
-// 어떻게 숫자 조합? -> dfs 재귀!
-
 import java.util.*;
 
 class Solution {
-    static HashSet<Integer> set = new HashSet<>(); // 중복 제거
-    static char[] arr; // 종이조각
-    static boolean[] visited; // 사용여부
-
+    HashSet<Integer> set = new HashSet<>();
+    
+    // 소수 체크
+    public boolean isPrime(int num){
+        // 1. 0과 1은 소수가 아님
+        if(num == 0 || num ==1){
+            return false;
+        }
+        // 2. 에라토스테네스의 체 limit 계산
+        int limit = (int)Math.sqrt(num); // 루트 씌운 값까지 계산
+        
+        // 3. 에라토스 체에 따라 limit까지만 배수 여부 확인
+        for(int i=2; i<limit + 1; i++){
+            if(num % i == 0)
+                return false;
+        }
+        return true;
+    }
+    
+    public void makeNum(String comb, String others){
+        // 1. 현재 조합을 set에 추가
+        if(!comb.equals(""))
+            set.add(Integer.valueOf(comb));
+        
+        for(int i=0; i<others.length(); i++){
+            // 2.남은 숫자 들 중 하나를 더해 새로운 조합 만듦.
+            makeNum(comb+others.charAt(i), others.substring(0,i) + others.substring(i+1));
+        }
+    }
     
     public int solution(String numbers) {
         int answer = 0;
-        arr = numbers.toCharArray(); // 배열로 저장
-        visited = new boolean[arr.length];
         
-        // 재귀함수
-        recur("", 0);
+        // 1. 숫자 조합 만들기 -> 재귀 사용
+        makeNum("", numbers);
+                
+        // 2. 소수 개수 세기
+        Iterator<Integer> iter = set.iterator();
         
-        answer = set.size();
-        return answer;
-    }
-    // dfs 재귀. 가능한 숫자조합 찾고 소수면 set에 추가
-    public void recur(String str, int idx){
-        int num;
-        if(str!=""){
-            num=Integer.parseInt(str);
-            if(isPrime(num)) set.add(num);
+        int cnt = 0;
+        while(iter.hasNext()){
+            int number = iter.next();
+            
+            if(isPrime(number))
+                cnt ++;
         }
-        if(idx==arr.length) return; // 재귀 종료조건: 끝까지 탐색 완료 시
         
-        for(int i=0; i<arr.length; i++){
-            if(visited[i]) continue; // 이미 썼다면 패스
-            // 그렇지 않다면
-            visited[i]=true;
-            recur(str+arr[i], idx+1); // 다음 재귀
-            visited[i]=false; //백 트래킹
-        }
-    }
-    
-    // 소수 판별
-    public boolean isPrime(int num){
-        if(num<2) return false;
-        for(int i=2; i<=Math.sqrt(num); i++){
-            if(num%i==0) return false;
-        }
-        return true;
+        // 3. 소수 개수 반환
+        return cnt;
     }
 }
