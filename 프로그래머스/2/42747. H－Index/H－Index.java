ec0cde70e20ba8d0adc40b23c5b964f2ead67221
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2025-01-21 23:10:09 +0900

    [level 2] Title: H-Index, Time: 4.56 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -5,13 +5,21 @@ class Solution {
         int answer = 0;
         
         // 1. 내림차순 정렬
-        Integer[] ci = Arrays.stream(citations).boxed().toArray(Integer[]::new);
         
-        Arrays.sort(ci, Collections.reverseOrder());
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int num : citations) {
+            list.add(num);
+        }
+        Collections.sort(list, Collections.reverseOrder());
+
+        
+        //Integer[] ci = Arrays.stream(citations).boxed().toArray(Integer[]::new);
+        
+        //Arrays.sort(ci, Collections.reverseOrder());
         
         // 2. 해당 논문의 인용 횟수 >= h 인지 체크
-        for(int i=0; i<ci.length; i++){
-            if(ci[i] >= i+1){
+        for(int i=0; i<list.size(); i++){
+            if(list.get(i) >= i+1){
                 answer=i+1;
             }else{
                 break;
