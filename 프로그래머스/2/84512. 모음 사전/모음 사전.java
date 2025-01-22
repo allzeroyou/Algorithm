618bd2c9c357be249698e7003b504acb8be9486b
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2025-01-22 21:44:48 +0900

    [level 2] Title: 모음 사전, Time: 6.90 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,25 +1,28 @@
-// 각 자리수마다 가능한 경우의 수를 계산
-// 주어진 단어의 각 글자에 대해, 그 글자 이전의 모든 경우의 수를 더하기
-// 마지막으로 단어의 위치를 계산
 import java.util.*;
 
 class Solution {
- // 모음 순서를 저장하는 배열
-    static String[] mo = {"A", "E", "I", "O", "U"};
-    // 각 자리수별 경우의 수를 저장하는 배열
-    static int[] WEIGHTS = {781, 156, 31, 6, 1};
+    String[] mo = {"A", "E", "I", "O", "U"};
+    List<String> dic = new ArrayList<>();
     
     public int solution(String word) {
         int answer = 0;
         
-        // 단어의 각 글자에 대해 반복
-        for (int i = 0; i < word.length(); i++) {
-            // 현재 글자의 인덱스를 찾음
-            int index = Arrays.asList(mo).indexOf(word.substring(i, i+1));
-            // 해당 글자 이전의 모든 경우의 수를 더함
-            answer += index * WEIGHTS[i] + 1;
-        }
+        // 1. 알파벳 조합 생성
+        recur("", 5);
         
-        return answer;
+        // 2. word의 인덱스+1 반환
+        return dic.indexOf(word)+1;
+    }
+    
+    private void recur(String cur, Integer maxLeng){
+        if(cur.length()>0){
+            dic.add(cur);
+        }
+        if(cur.length() == maxLeng){
+            return;
+        }
+        for(String m: mo){
+            recur(cur+m, maxLeng);
+        }
     }
 }
