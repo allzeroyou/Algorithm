62bc957af1d21d1b44ce59b686190eeacc8077cc
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2025-01-22 19:24:54 +0900

    [level 2] Title: 피로도, Time: 5.00 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,27 +1,32 @@
 import java.util.*;
 
 class Solution {
-    static int answer;
-    static boolean[] visited;
+    private int maxExplored;
     
-    // 현재피로도:k, 소모 피로도: dundeons
     public int solution(int k, int[][] dungeons) {
-        answer = 0;
-        // 변수 세팅
-        visited = new boolean[dungeons.length];
-        dfs(k, dungeons, 0);
+        int answer = -1;
         
+        boolean[] visited = new boolean[dungeons.length]; // 방문 배열
+        recur(k, dungeons, visited, 0);
         
-        return answer;
+        return maxExplored;
     }
-    public void dfs(int k, int[][] dungeons, int cnt){
+    
+    private void recur(int k, int[][] dungeons, boolean[] visited, int cnt ){
+        
+        // 탐험
         for(int i=0; i<dungeons.length; i++){
-            if(!visited[i] && dungeons[i][0] <= k){
+            // 방문 x && 현재 피로도 >= 최소 피로도
+            if(!visited[i] && k>= dungeons[i][0]){
+                // 방문 체크
                 visited[i]=true;
-                dfs(k- dungeons[i][1], dungeons, cnt+1);
+                // 현재 피로도, 방문체크, 탐험횟수+1 갱신한 재귀 함수 실행
+                recur(k-dungeons[i][1], dungeons, visited,cnt+1 );
+                // 실행 후 방문체크 되돌려 다른 순서 탐험 가능하도록.
                 visited[i]=false;
             }
+            // 최댓값 구하기
+            maxExplored = Math.max(cnt, maxExplored);
         }
-        answer = Math.max(answer, cnt);
     }
 }
