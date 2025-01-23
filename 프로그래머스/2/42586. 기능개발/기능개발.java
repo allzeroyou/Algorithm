bbc071884127e209efe4467214fda76fa85b01ad
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2025-01-23 20:59:33 +0900

    [level 2] Title: 기능개발, Time: 2.62 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = {};
+        // 값 수정이 가능하게끔 arraylist로
+        List<Integer> ans = new ArrayList<>();
+    
+        // 배포 가능한 날짜
+        List<Integer> upload = new ArrayList<>();
+        
+        for(int i=0; i<progresses.length; i++){
+            upload.add((int)Math.ceil((100.0-progresses[i])/speeds[i]));
+        }
+        
+        // 앞의 기능
+        int pre = upload.get(0);
+        int cnt = 1; // 함께 배포하는 작업 수
+        
+        for(int i=1; i<upload.size(); i++){
+            // 앞의 배포 날짜가 더 크다면, 배포 날짜에 함께 배포한다
+            if(upload.get(i) <= pre){
+                cnt ++;
+            }else{
+                ans.add(cnt);
+                cnt = 1;
+                pre= upload.get(i); // 앞의 날짜 갱신
+            }
+        }
+        ans.add(cnt); // 마지막 그룹까지 추가
+        
+        return ans.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
