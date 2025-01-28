e3a021ff913da9e7c510b510fb82f1fd7c3e6fa7
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2025-01-28 15:53:40 +0900

    [Silver III] Title: 2×n 타일링, Time: 104 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,17 +1,34 @@
-
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        long dp[] = new long[1001];
-        // dp 테이블 초기화
-        dp[1] = 1; // n=1 일때 타일 채우는 경우의 수
-        dp[2] = 2; // n=2 일때 타일 채우는 경우의수
-        for (int i = 3; i <= n; i++) {
-            dp[i] = (dp[i - 1] + dp[i - 2]) % 10007; // 할때마다 나누는거랑 맨 마지막에 나누는거랑 결과 같음.
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // 2xn 크기
+        int n = Integer.parseInt(br.readLine());
+
+        // 1. dp 테이블 설정
+        int[] dp = new int[n + 1];
+
+        // 3. 초기값 설정
+        // 1인 경우 예외처리 해준다!
+        if (n == 1) {
+            dp[1] = 1;
+            System.out.println(dp[n]);
+            return;
         }
+
+        dp[1] = 1;
+        dp[2] = 2;
+
+        // 2. 점화식 도출
+        // int overflow가 발생하지 않도록 결과 반환시에만 10007을 나눠주는 것이 아닌 계산마다 나눈 값을 저장해야 한다!
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % 10007;
+        }
+
+        // 답 도출
         System.out.println(dp[n]);
     }
 }
