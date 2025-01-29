49ff54e4a782d7c58ed0680a3a9746941d5d8282
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2025-01-29 13:11:20 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1108 ms, Memory: 58720 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,33 +1,42 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        // 띄어쓰기 구분
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        // n: 개수, m: 합을 구해야 하는 횟수
+        // n m
+        String ln1 = br.readLine();
+        StringTokenizer st = new StringTokenizer(ln1, " ");
+
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        // 숫자 배열
-        long[] arr = new long[n];
-        // 합 배열
-        long[] sum_arr = new long[n + 1];
 
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-            sum_arr[i + 1] = sum_arr[i] + arr[i];
+        // 1. dp 테이블 설정
+        int[] dp = new int[n + 1];
+        String ln2 = br.readLine();
+        StringTokenizer st2 = new StringTokenizer(ln2, " ");
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i - 1] + Integer.parseInt(st2.nextToken());
         }
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int s = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
-            // 구간합 공식
-            System.out.println(sum_arr[e] - sum_arr[s - 1]);
+
+        // 3. 초기값 설정
+        // 1인 경우 예외처리 해준다!
+        if (n == 1) {
+            System.out.println(dp[1]);
+            return;
+        }
+
+        // 2. 점화식 도출: 구간 합 구하기
+        for (int k = 0; k < m; k++) {
+            String ln3 = br.readLine();
+            StringTokenizer st3 = new StringTokenizer(ln3, " ");
+            int i = Integer.parseInt(st3.nextToken());
+            int j = Integer.parseInt(st3.nextToken());
+
+            // 답 도출
+            System.out.println(dp[j] - dp[i - 1]);
         }
     }
 }
