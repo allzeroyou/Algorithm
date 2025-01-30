c980c4bccd46cc361bce072f7ebb6fb91d9ed5d8
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2025-01-30 15:22:15 +0900

    [Silver I] Title: 미로 탐색, Time: 116 ms, Memory: 15052 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,61 +1,65 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    // 상하좌우 이동
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {-1, 0, 1, 0};
-    // 방문 배열
-    static boolean[][] visited;
-    // 탐색 board
-    static int[][] board;
-    // 가로 x 세로
-    static int n, m;
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        board = new int[n][m];
-        visited = new boolean[n][m];
+
+        // 행, 열
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[][] board = new int[n][m];
+
+        // 거리 배열
+        int[][] dist = new int[n][m];
 
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            // 공백 x
-            String line = st.nextToken(); // 101111
             for (int j = 0; j < m; j++) {
-                board[i][j] = Integer.parseInt(line.substring(j, j + 1));
+                dist[i][j] = -1; // -1 로 초기화함으로서 방문 배열 따로 두지 x
             }
         }
-        // BFS
-        BFS(0, 0); // 시작점
-        System.out.println(board[n - 1][m - 1]); // idx
-    }
 
-    private static void BFS(int i, int j) {
+        for (int i = 0; i < n; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < m; j++) {
+                board[i][j] = line.charAt(j) - '0';
+            }
+        }
+        // System.out.println(Arrays.deepToString(board));
+
+        // bfs 수행하면서 거리 저장
         Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{i, j}); // (0,0)
+
+        int[] dx = {1, -1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+
+        // 시작점을 큐에 넣는다
+        q.offer(new int[]{0, 0}); // board: 0-based
+        // 시작점을 방문 체크한다!
+        dist[0][0] = 1;
+
         while (!q.isEmpty()) {
-            int now[] = q.poll();
-            visited[i][j] = true;
-            for (int k = 0; k < 4; k++) { // 상하좌우 탐색
-                int x = now[0] + dx[k];
-                int y = now[1] + dy[k];
-                if (x >= 0 && y >= 0 && x < n && y < m) { // 배열 안이거나
-                    if (board[x][y] != 0 && !visited[x][y]) {
-                        // 칸이 1이거나 방문 하지 않았을 경우
-                        visited[x][y] = true;
-                        board[x][y] = board[now[0]][now[1]] + 1; // 최단경로 갱신
-                        q.add(new int[]{x, y});
+
+            int[] cur = q.poll();
+            int cx = cur[0];
+            int cy = cur[1];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+
+                if (nx >= 0 && nx < n && ny >= 0 && ny < m) {
+                    if (dist[nx][ny] == -1 && board[nx][ny] == 1) {
+                        q.offer(new int[]{nx, ny});
+
+                        // 거리 갱신(현재까지의 거리 + 1)
+                        dist[nx][ny] = dist[cx][cy] + 1;
                     }
                 }
             }
         }
+        System.out.println(dist[n - 1][m - 1]);
     }
 }
