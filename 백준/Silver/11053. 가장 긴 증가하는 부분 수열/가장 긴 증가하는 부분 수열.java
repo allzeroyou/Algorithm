597a79fb5f4daad933480eb505323cb14d72c77a
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2025-02-16 16:02:16 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 124 ms, Memory: 14564 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -9,26 +9,24 @@ public class Main {
 
         // 수열
         int[] a = new int[n];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         for (int i = 0; i < n; i++) {
             a[i] = Integer.parseInt(st.nextToken());
         }
-
-        // 가장 긴 증가하는 부분 수열
+        // dp 테이블: n일때 최대길이
         int[] dp = new int[n + 1];
-        Arrays.fill(dp, 1); // 모든 원소의 최소 길이 = 1
+        int ans = 1; // 초기값
+        Arrays.fill(dp, 1); // 모든 수열의 요소는 각 1의 길이를 가짐
 
-        // 초기값
-        int ans = 1; // 초항은 무조건 카운트
-
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (a[j] < a[i]) { // 최장 길이
+        for (int i = 1; i < n; i++) {// 1-based
+            for (int j = 0; j < i; j++) { // 수열이 증가해야 하므로 2중 for문 사용한다
+                if (a[i] > a[j]) {
                     dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
+                ans = Math.max(ans, dp[i]);
             }
-            ans = Math.max(dp[i], ans);
         }
         System.out.println(ans);
     }
