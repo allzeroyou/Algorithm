efa0c16bad49d1aadc1e961a83c51f6ea2a60113
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2025-02-16 16:44:31 +0900

    [Silver IV] Title: 수 찾기, Time: 1072 ms, Memory: 54320 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,42 +1,53 @@
-
-import java.io.BufferedReader;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = sc.nextInt();
-        }
-        // 데이터 정렬 -> 이진탐색 준비
-        Arrays.sort(arr);
+    static int[] a;
+    static int[] b;
+    static int n, m;
 
-        int m = sc.nextInt();
+    static int biSearch(int target) {
+        int st = 0;
+        int en = n - 1;
+
+        while (st <= en) { // 이분 탐색 종료 조건: 해당 값이 배열에 없을 경우 -> st, en이 역전됨.
+            int mid = (st + en) / 2;
+
+            if (a[mid] < target) st = mid + 1;
+            else if (a[mid] > target) en = mid - 1;
+            else return 1; // 찾은 경우 1 반환 후 종료
+        }
+        // st > en 일 경우 배열에 없음!
+        return 0;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine()); // 배열 요소 개수
+
+        // 배열 a
+        a = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+
+        m = Integer.parseInt(br.readLine()); // 찾으려는 숫자 개수
+        // 배열 b
+        b = new int[m];
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
-            boolean find = false;
-            int target = sc.nextInt();
-            int start = 0;
-            int end = arr.length - 1;
-            while (start <= end) {
-                int mid_idx = (start + end) / 2;
-                int mid_val = arr[mid_idx];
-                if (mid_val > target) {
-                    end = mid_idx - 1;
-                } else if (mid_val < target) {
-                    start = mid_idx + 1;
-                } else {
-                    find = true;
-                    break;
-                }
-            }
-            if (find) {
-                System.out.println(1);
-            } else {
-                System.out.println(0);
-            }
+            b[i] = Integer.parseInt(st.nextToken());
+        }
+
+        // 이분탐색 로직 구현
+        // 배열-오름차순 정렬
+        Arrays.sort(a);
+
+        for (int i = 0; i < m; i++) {
+            int ans = biSearch(b[i]);
+            System.out.println(ans);
         }
     }
 }
