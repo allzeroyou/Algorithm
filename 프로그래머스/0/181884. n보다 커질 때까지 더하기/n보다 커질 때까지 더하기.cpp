777c2fbc87a7cbeabc1f9c8bcd5b9e435e1ecced
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2025-08-21 07:59:11 +0900

    [level 0] Title: n보다 커질 때까지 더하기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers, int n) {
+    int answer = 0;
+    
+    // n보다 커질때 그때의 sum return
+    for(int i=0; i<numbers.size(); i++){
+        if(answer<=n){
+            answer+=numbers[i];
+        }else break;
+    }
+    return answer;
+}
