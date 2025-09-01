cf9fc76f086a2cbdec2b25400a7a43edf5f9234b
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2025-09-02 07:59:29 +0900

    [level 0] Title: 배열에서 문자열 대소문자 변환하기, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/181875. 배열에서 문자열 대소문자 변환하기/배열에서 문자열 대소문자 변환하기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+vector<string> solution(vector<string> strArr) {
+    vector<string> answer;
+    // 홀수 인덱스 -> 대문자로
+    // 짝수 인덱스 -> 소문자로
+    for(int i=0; i<strArr.size(); i++){
+        if(i%2==0){ 
+            transform(strArr[i].begin(), strArr[i].end(), strArr[i].begin(), ::tolower);
+        }else{
+            transform(strArr[i].begin(), strArr[i].end(), strArr[i].begin(), ::toupper);
+        }
+    }
+
+    return strArr;
+}
