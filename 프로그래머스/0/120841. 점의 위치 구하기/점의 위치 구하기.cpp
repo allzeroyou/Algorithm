fd92a4d79fcb7725dcd7566603121efcca30ffd2
Dayeong Yoo <0fcoursewhynot@duksung.ac.kr>
2026-01-12 08:32:18 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.02 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/120841. 점의 위치 구하기/점의 위치 구하기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> dot) {
+    int answer = 0;
+    // 사분면 알아내기
+    if(dot[0]>0 && dot[1]>0)
+    {
+        return 1;
+    }else if(dot[0]>0 && dot[1]<0){
+        return 4;
+    }else if(dot[0]<0 && dot[1]>0){
+        return 2;
+    }else{
+        return 3;
+    }
+}
